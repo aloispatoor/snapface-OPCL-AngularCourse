cd830aed356b71119ad570557dfa23cea0f6baf5
Alois Patoor <palois89@posteo.net>
2022-09-21 13:18:12 +0200

    better code management

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -1,12 +1,4 @@
 export class FaceSnap {
-    // constructor(public title: string,
-    //             public description: string,
-    //             public imageUrl: string,
-    //             public createdDate: Date,
-    //             public snaps: number,
-    //             public location?: string) {
-    // }
-
     title!: string;
     description!: string;
     imageUrl!: string;
